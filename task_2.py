33e64087ad5ca22701b092cbe907587e5b884442
abnatop <abnatop@gmail.com>
2020-11-24 11:26:53 +0900

    Задача 2

--- task_2.py
+++ task_2.py
@@ -2,3 +2,23 @@
 # с индексами 0 и 1, 2 и 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте.
 # Для заполнения списка элементов необходимо использовать функцию input().
 
+managed_list = []
+
+while True:
+    list_element = input('Введите элемент списка (или END для выхода): ')
+    if list_element != 'END':
+        managed_list.append(list_element)
+    else:
+        break
+
+if len(managed_list) < 2:
+    print('Кол-во элементов меньше двух, обмен значениями невозможен')
+    exit()
+
+print('Исходный список: ', managed_list)
+
+for i in range(0, len(managed_list) - 1, 2):
+    j = i + 1
+    managed_list[i], managed_list[j] = managed_list[j], managed_list[i]
+
+print('Измененный список: ', managed_list)
